69aad53ff4220e4d9172f6e21c1352d899b7f647
Miguel Angel Lopez <miguelanglb@outlook.com>
2025-05-05 22:08:42 -0600

    Primer commit - Proyecto Visual Studio

---
+++ Restaurant/View/LoginView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Restaurant.View
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btn_cerrar_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Screen_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Screen_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
